c05cd2d46e87bca1277fe509da1bef6d4590ef11
p.tamkovich <p.tamkovich@samsung.com>
2019-01-18 16:28:40 +0100

    added Cart

---
+++ SchoolPrj/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolPrj.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public int AddToCart(Guid id)
+        {
+            Session["cart"] += id.ToString()+",";
+            string[] goodsInCart = Session["cart"].ToString().Split(',');
+            return goodsInCart.Length-1;
+        }
+    }
+}
